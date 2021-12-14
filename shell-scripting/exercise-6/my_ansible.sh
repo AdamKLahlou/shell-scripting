6f5cc472b0c3bee2b8daf24ced7f7bb25ff118ac
lorin fawaz kakahoun <lorin-fawaz.kakahoun@edu.devinci.fr>
2021-12-15 00:55:50 +0100

    exercise-6

---
+++ shell-scripting/exercise-6/my_ansible.sh
@@ -0,0 +1,12 @@
+mkdir documents
+touch meeting_note.txt
+cd documents
+mkdir gamez images work
+cd work
+mkdir code plannings
+cd plannings
+touch april.xlsx febuary.xlsx january.xlsx march.xlsx
+cd /home/lorin/Documents/gamez
+touch csgo.exe
+chmod 764 csgo.exe
+exit 0
