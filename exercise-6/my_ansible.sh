120a49a859ecb703549bdb2e18cb23627b8cad49
Adam Kassi Lahlou <kassilahlouadam@gmail.com>
2021-12-15 01:38:38 +0100

    Exercice 6

---
+++ exercise-6/my_ansible.sh
@@ -0,0 +1,10 @@
+mkdir documents
+touch meeting_notes.txt
+cd documents
+mkdir gamez images work 
+cd plannings
+touch april.xlsx february.xlsx january.xlsx march.xlsx
+cd /home/lorin/Documents/gamez
+touch csgo.exe
+chmod 764 csgo.exe
+exit 0
